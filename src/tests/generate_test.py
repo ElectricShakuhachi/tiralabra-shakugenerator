f867d10dbb356883be965290c9f06f0c318f6fb8
ElectricShakuhachi <haavistoaaro@gmail.com>
2022-03-24 21:07:12 +0200

    add tests for generate class

--- src/tests/generate_test.py
+++ src/tests/generate_test.py
@@ -62,96 +62,90 @@ class TestShakuGenerator(unittest.TestCase):
         for i in range(2, 100):
             pitches.append(i)
             lenghts.append(i)
-        for pitch, lenght in [(1, 2), (5, 2), (10, 4), (8, 2)]:
+        for pitch, lenght in [(6, 1), (1, 2), (5, 2), (10, 4), (8, 2)]: # set 1
+            pitches.append(pitch)
+            lenghts.append(lenght)
+        for pitch, lenght in [(101, 2), (8, 8), (2, 16), (9, 4), (101, 2), (8, 8), (2, 16), (4, 2)]: # set 2
+            pitches.append(pitch)
+            lenghts.append(lenght)
+        for pitch, lenght in [(102, 9), (60, 16), (20, 32), (38, 16), 
+                                (102, 9), (60, 16), (20, 32), (38, 16), 
+                                (102, 9), (60, 16), (20, 32), (48, 2),]: # set 3
             pitches.append(pitch)
             lenghts.append(lenght)
         self.generator.pitch_trie.feed_data([pitches])
         self.generator.lenght_trie.feed_data([lenghts])
 
     def test_generate_note_always_returns_the_right_note_if_there_is_just_one_match_in_trie(self):
-        values = []
+        """This test case reflects if algorithm will always return the matched note
+        if only one note matches as following the given sequence in trie.
+        Utilizes set 1 in _make_defined_trie -method"""
+        values = set()
         previous = {"pitches": [1, 5, 10], "lenghts": [2, 2, 4]}
         for i in range(50):
             self.generator = ShakuGenerator()
             self._make_defined_trie()
-            values.append(self.generator.generate_note(previous))
-        pitch_count = 0
-        lenght_count = 0
-        for note in values:
-            if note[0] == 8:
-                pitch_count += 1
-            if note[1] == 2:
-                lenght_count += 1
-        self.assertEqual(pitch_count, 50)
+            values.add(self.generator.generate_note(previous)[0])
+        self.assertEqual(len(values) == 1 and 8 in values, True)
 
-    # def test_generate_note_returns_one_integer_if_three_previous_with_match(self):
-    #     self.assertIsInstance(self.generator.generate_note([65,63,62]), int)
+    def test_generate_note_generates_note_based_on_last_three_if_four_previous(self):
+        self._make_defined_trie()
+        previous = {"pitches": [8, 1, 5, 10], "lenghts": [8, 2, 2, 4]}
+        self.assertEqual((8, 2), self.generator.generate_note(previous))
 
-    # def test_generate_note_returns_error_if_four_previous_given(self):
-    #     self.assertRaises(ValueError, self.generator.generate_note, ([65,63,62,63]))
+    def test_generate_note_returns_random_integer_from_data_if_zero_match_one_prev(self):
+        values = set()
+        previous = {"pitches": [101], "lenghts": [100]}
+        for i in range(5000):
+            values.add(self.generator.generate_note(previous))
+        self.assertGreater(len(values), 100)
 
-    # def test_generate_note_returns_error_if_five_previous_given(self):
-    #     self.assertRaises(ValueError, self.generator.generate_note, ([65,63,62,62,63]))
+    def test_generate_note_returns_both_possibilities_on_two_matches_test_one(self):
+        """Utilizes set 2 in _make_defined_trie -method"""
+        values = set()
+        previous = {"pitches": [101, 8, 2], "lenghts": [2, 8, 16]}
+        for i in range(50):
+            self.generator = ShakuGenerator()
+            self._make_defined_trie()
+            values.add(self.generator.generate_note(previous)[0])
+        self.assertEqual(len(values) == 2 and 9 in values and 4 in values, True)
 
-    # def test_generate_note_returns_random_integer_from_data_if_zero_match_one_prev(self):
-    #     values = set()
-    #     for i in range(50000):
-    #         values.add(self.generator.generate_note([93]))
-    #     self.assertEqual(len(values), 9)
+    def test_generate_note_returns_more_represented_note_more_often(self):
+        """Utilizes set 3 in _make_defined_trie -method
+        Shows that more represented sequence in trie is more likely to be followed
+        This test also shows how pitch and trie generation is independent"""
+        values = {}
+        previous = {"pitches": [102, 60, 20], "lenghts": [60, 10, 20]}
+        for i in range(50):
+            self.generator = ShakuGenerator()
+            self._make_defined_trie()
+            note = self.generator.generate_note(previous)[0]
+            if note in values:
+                values[note] += 1
+            else:
+                values[note] = 1
+        self.assertGreater(values[38], values[48])
 
-    # def test_generate_note_returns_random_integer_from_data_if_zero_match_two_prev(self):
-    #     values = set()
-    #     for i in range(50000):
-    #         values.add(self.generator.generate_note([93,91]))
-    #     self.assertEqual(len(values), 9)
-
-    # def test_generate_note_returns_correct_note_on_one_match_test_one(self):
-    #     note = self.generator.generate_note([62,65,65])
-    #     self.assertEqual(note, 63)
-
-    # def test_generate_note_returns_correct_note_on_one_match_test_two(self):
-    #     note = self.generator.generate_note([62,62,63])
-    #     self.assertEqual(note, 64)
-
-    # def test_generate_note_returns_correct_note_on_one_match_two_prev(self):
-    #     note = self.generator.generate_note([62,62])
-    #     self.assertEqual(note, 63)
-
-    # def test_generate_note_returns_both_possibilities_on_two_matches_test_one(self):
-    #     values = set()
-    #     for i in range(100):
-    #         values.add(self.generator.generate_note([63, 63, 63]))
-    #     self.assertEqual(61 in values and 80 in values, True)
-
-    # def test_generate_note_returns_two_different_values_on_two_matches_test_one(self):
-    #     values = set()
-    #     for i in range(100):
-    #         values.add(self.generator.generate_note([63, 63, 63]))
-    #     self.assertEqual(len(values), 2)
-
-    # def test_generate_note_returns_more_likely_value_more_often(self):
-    #     count_80 = 0
-    #     count_61 = 0
-    #     for i in range(100):
-    #         value = self.generator.generate_note([63, 63, 63])
-    #         if value == 80:
-    #             count_80 += 1
-    #         elif value == 61:
-    #             count_61 += 1
-    #     self.assertGreater(count_80, count_61)
-
-    # def test_generate_note_exact_variance_differs_per_run(self):
-    #     count_80_first = 0
-    #     count_80_second = 0
-    #     for i in range(10000):
-    #         value = self.generator.generate_note([63, 63, 63])
-    #         if value == 80:
-    #             count_80_first += 1
-    #     for i in range(10000):
-    #         value = self.generator.generate_note([63,63,63])
-    #         if value == 80:
-    #             count_80_second += 1
-    #     self.assertNotEqual(count_80_first, count_80_second)
-
-    # def test_generate_note_returns_exact_match_to_second_level_one_match(self):
-    #     self.assertEqual(self.generator.generate_note([90,90,67]), 89)
+    def test_generate_note_exact_variance_differs_per_run(self):
+        """Utilizes set 3 in _make_defined_trie method
+        Shows that note generation is random to a degree"""
+        values1 = {}
+        previous = {"pitches": [102, 60, 20], "lenghts": [60, 10, 20]}
+        for i in range(1000):
+            self.generator = ShakuGenerator()
+            self._make_defined_trie()
+            note = self.generator.generate_note(previous)[0]
+            if note in values1:
+                values1[note] += 1
+            else:
+                values1[note] = 1
+        values2 = {}
+        for i in range(1000):
+            self.generator = ShakuGenerator()
+            self._make_defined_trie()
+            note = self.generator.generate_note(previous)[0]
+            if note in values2:
+                values2[note] += 1
+            else:
+                values2[note] = 1
+        self.assertNotEqual(values1[38], values2[38])
